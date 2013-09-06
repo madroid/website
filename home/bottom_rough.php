9d58446d22da9749058fa87799397a9e57969e52
madroid <cs1110250@gmail.com>
2013-09-06 13:13:41 +0530

    column layout working fine for home page :-)

---
+++ home/bottom_rough.php
@@ -0,0 +1,41 @@
+		<div id="bottom">
+			<div id="video">
+				<img src="../layout/video.png" alt="Beyond Pink Video"/>
+			</div>
+
+			<div id="img1" class="item">
+				<div>
+					<img src="../img/small/image1.jpg" class="img">
+				</div>	
+				<div class="title">
+					<img src="../layout/heart.png"/>
+				</div>
+				<hr>
+				<div class="description">
+					<div class="colors">
+						<div class="circle" style="background-color:#F26836"></div>
+						<div class="circle" style="background-color:#95948F"></div>
+						<div class="circle" style="background-color:#F86FED"></div>
+						<div class="circle" style="background-color:#E4E13F"></div>
+					</div>
+				</div>	
+			</div>
+			
+			<div id="img2" class="item">
+				<div>
+					<img src="../img/small/image2.jpg" class="img">
+				</div>	
+				<div class="title">
+					<img src="../layout/heart.png"/>
+				</div>
+				<hr>
+				<div class="description">
+					<div class="colors">
+						<div class="circle" style="background-color:#F26836"></div>
+						<div class="circle" style="background-color:#95948F"></div>
+						<div class="circle" style="background-color:#F86FED"></div>
+						<div class="circle" style="background-color:#E4E13F"></div>
+					</div>
+				</div>	
+			</div>
+		</div>	
--- home/home2.php
+++ home/home2.php
@@ -12,7 +12,6 @@
 			var window_width = $(window).width();
 			var window_height = $(window).height();
 			$("#top").height(0.95*window_height);
-//			$("body").width(window_width-10);
 		
 		});
 	</script>
@@ -47,47 +46,116 @@
 		<?php   include_once("footer.php");	   ?>
 		<?php include_once("login_modal.php"); ?>
 <!-------------------------------------------------------------------------------->
-
-
-		<div id="bottom">
-			<div id="video">
-				<img src="../layout/video.png" alt="Beyond Pink Video"/>
-			</div>
-			<div id="img1" class="item">
-				<div>
-					<img src="../img/small/image1.jpg" class="img">
-				</div>	
-				<div class="title">
-					<img src="../layout/heart.png"/>
-				</div>
-				<hr>
-				<div class="description">
-					<div class="colors">
-						<div class="circle" style="background-color:#F26836"></div>
-						<div class="circle" style="background-color:#95948F"></div>
-						<div class="circle" style="background-color:#F86FED"></div>
-						<div class="circle" style="background-color:#E4E13F"></div>
-					</div>
-				</div>	
-			</div>
-			<div id="img2" class="item">
-				<div>
-					<img src="../img/small/image2.jpg" class="img">
-				</div>	
-				<div class="title">
-					<img src="../layout/heart.png"/>
-				</div>
-				<hr>
-				<div class="description">
-					<div class="colors">
-						<div class="circle" style="background-color:#F26836"></div>
-						<div class="circle" style="background-color:#95948F"></div>
-						<div class="circle" style="background-color:#F86FED"></div>
-						<div class="circle" style="background-color:#E4E13F"></div>
-					</div>
-				</div>	
-			</div>
-		</div>	
 	
+	<div id="bottom">
+		<table width="100%" border="0">
+			<tr class="td">
+				<td id="col_left">
+					<div id="video">
+						<img src="../layout/video.png" alt="Beyond Pink Video"/>
+					</div>
+					<div id="non_video">
+						<div id="img1" class="item">
+							<div>
+								<img src="../img/small/image3.jpg" class="img">
+							</div>	
+							<div class="title">
+								<img src="../layout/heart.png"/>
+							</div>
+							<hr>
+							<div class="description">
+								<div class="colors">
+									<div class="circle" style="background-color:#F26836"></div>
+									<div class="circle" style="background-color:#95948F"></div>
+									<div class="circle" style="background-color:#F86FED"></div>
+									<div class="circle" style="background-color:#E4E13F"></div>
+								</div>
+							</div>	
+						</div>
+					</div>	
+
+				</td>
+				
+				<td id="col_middle">
+					<div id="img1" class="item">
+						<div>
+							<img src="../img/small/image1.jpg" class="img">
+						</div>	
+						<div class="title">
+							<img src="../layout/heart.png"/>
+						</div>
+						<hr>
+						<div class="description">
+							<div class="colors">
+								<div class="circle" style="background-color:#F26836"></div>
+								<div class="circle" style="background-color:#95948F"></div>
+								<div class="circle" style="background-color:#F86FED"></div>
+								<div class="circle" style="background-color:#E4E13F"></div>
+							</div>
+						</div>	
+					</div>
+
+						<div id="img0" class="item">
+							<div>
+								<img src="../img/small/image4.jpg" class="img">
+							</div>	
+							<div class="title">
+								<img src="../layout/heart.png"/>
+							</div>
+							<hr>
+							<div class="description">
+								<div class="colors">
+									<div class="circle" style="background-color:#F26836"></div>
+									<div class="circle" style="background-color:#95948F"></div>
+									<div class="circle" style="background-color:#F86FED"></div>
+									<div class="circle" style="background-color:#E4E13F"></div>
+								</div>
+							</div>	
+						</div>
+
+				</td>
+				
+				<td id="col_right">
+					
+					<div id="img2" class="item">
+						<div>
+							<img src="../img/small/image2.jpg" class="img">
+						</div>	
+						<div class="title">
+							<img src="../layout/heart.png"/>
+						</div>
+						<hr>
+						<div class="description">
+							<div class="colors">
+								<div class="circle" style="background-color:#F26836"></div>
+								<div class="circle" style="background-color:#95948F"></div>
+								<div class="circle" style="background-color:#F86FED"></div>
+								<div class="circle" style="background-color:#E4E13F"></div>
+							</div>
+						</div>	
+					</div>
+
+					<div id="img0" class="item">
+							<div>
+								<img src="../img/small/image5.jpg" class="img">
+							</div>	
+							<div class="title">
+								<img src="../layout/heart.png"/>
+							</div>
+							<hr>
+							<div class="description">
+								<div class="colors">
+									<div class="circle" style="background-color:#F26836"></div>
+									<div class="circle" style="background-color:#95948F"></div>
+									<div class="circle" style="background-color:#F86FED"></div>
+									<div class="circle" style="background-color:#E4E13F"></div>
+								</div>
+							</div>	
+						</div>
+
+				</td>
+			</tr>	
+		<table>	
+	</div>
 	</body>
 </html>
